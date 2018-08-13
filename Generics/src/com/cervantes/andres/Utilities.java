ee38cadf5bc078dd09c140c450798528410b8f72
Andres C Developer <acervantes107@gmail.com>
2018-08-12 19:38:25 -0500

    Added a utilities class with a generic method for testing generics

---
+++ Generics/src/com/cervantes/andres/Utilities.java
@@ -0,0 +1,12 @@
+package com.cervantes.andres;
+
+import java.util.List;
+
+public class Utilities {
+
+	public static <T> void fill(List<T> list, T val) {
+		for( int i = 0; i < list.size(); i++ ) {
+			list.set(i, val);
+		}
+	}
+}
---
+++ Generics/src/com/cervantes/andres/UtilitiesTest.java
@@ -0,0 +1,17 @@
+package com.cervantes.andres;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UtilitiesTest {
+
+	public static void main(String[] args) {
+		List<Integer> intList = new ArrayList<>();
+		intList.add(10);
+		intList.add(20);
+		System.out.println( "The original list is:" + intList );
+		Utilities.fill(intList, 100 );
+		System.out.println( "The list after calling Utilities.fill() is:" + intList );
+	}
+
+}
